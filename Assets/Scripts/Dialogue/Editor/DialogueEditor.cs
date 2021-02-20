a6b7055dd182ee99f58c8ac77741081bfaf10f34
Gilderko <martin.sepela007@gmail.com>
2021-02-20 19:07:29 +0100

    Editing Dynamic Data

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -9,6 +9,7 @@ namespace RPG.Dialogue.Editor
     public class DialogueEditor : EditorWindow
     {
         Dialogue selectedDialogue = null;
+        string customText = "Bob";
 
         [MenuItem("Window/Dialogue Editor")] // Annotation Callback in editor
         public static void ShowEditorWindow()
@@ -42,8 +43,7 @@ namespace RPG.Dialogue.Editor
         {
             Dialogue selectedObject = Selection.activeObject as Dialogue;
             if (selectedObject != null)
-            {
-                Debug.Log("New Dialogue");
+            {                
                 selectedDialogue = selectedObject;                
             }            
         }
@@ -54,13 +54,18 @@ namespace RPG.Dialogue.Editor
             OnChangeDialogue();
             if (selectedDialogue == null)
             {
-                EditorGUILayout.LabelField("No dialogue selected.");
+                EditorGUILayout.TextField("No dialogue selected.");
             }
             else
             {
                 foreach (DialogueNode node in selectedDialogue.GetAllNodes())
                 {
-                    EditorGUILayout.LabelField(node.text);
+                    string newText = EditorGUILayout.TextField(node.text); // OnGUI gets called twice, first return text inside and second changes it
+                    if (newText != node.text)
+                    {
+                        node.text = newText;
+                        EditorUtility.SetDirty(selectedDialogue);
+                    }                    
                 }
             }
         }
