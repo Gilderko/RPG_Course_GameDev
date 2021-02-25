47d3c578b6b0521301df90e37dd39952ee661864
Gilderko <martin.sepela007@gmail.com>
2021-02-25 18:53:30 +0100

    Dialogue Architecture

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -24,7 +24,7 @@ namespace RPG.Dialogue
             {
                 nodeLookup[node.name] = node;
             }
-        }
+        }        
 
         public IEnumerable<DialogueNode> GetAllNodes()
         {
---
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Dialogue;
+using TMPro;
+
+namespace RPG.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        PlayerConversant playerConversant;
+        [SerializeField] TextMeshProUGUI AIText;
+
+        void Start()
+        {
+            playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            AIText.text = playerConversant.GetText();
+        }
+    }
+}
