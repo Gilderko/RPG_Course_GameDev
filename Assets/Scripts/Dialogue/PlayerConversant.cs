1bfe7ee3137292f286b2589182751ae801491335
Gilderko <martin.sepela007@gmail.com>
2021-02-25 21:12:01 +0100

    Building Choice list

--- Assets/Scripts/Dialogue/PlayerConversant.cs
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -1,21 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace RPG.Dialogue
 {
     public class PlayerConversant : MonoBehaviour
     {
         [SerializeField] Dialogue currentDialogue;
+        DialogueNode currentNode;
+
+        private void Awake()
+        {
+            currentNode = currentDialogue.GetRootNode();
+        }
 
         public string GetText()
         {
-            if (currentDialogue == null)
+            if (currentNode == null)
             {
                 return "";
             }
+           return currentNode.GetText();
+        }
 
-           return currentDialogue.GetRootNode().GetText();
+        public void Next()
+        {
+            DialogueNode[] currentNodeChildren = currentDialogue.GetAllChildren(currentNode).ToArray<DialogueNode>();
+            int newNextIndex = Random.Range(0, currentNodeChildren.Length);
+            currentNode = currentNodeChildren[newNextIndex];
+        }
+
+        public bool HasNext()
+        {            
+            return currentDialogue.GetAllChildren(currentNode).Count() > 0;
+        }
+
+        public IEnumerable<string> GetChoices()
+        {
+            yield return "Something";
+            yield return "Nothing";
+            yield return "Everything";
         }
     }
 }
--- Assets/Scripts/UI/DialogueUI.cs
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPG.Dialogue;
 using TMPro;
+using UnityEngine.UI;
 
 namespace RPG.UI
 {
@@ -10,11 +11,41 @@ namespace RPG.UI
     {
         PlayerConversant playerConversant;
         [SerializeField] TextMeshProUGUI AIText;
+        [SerializeField] Button nextButton;
+
+        [SerializeField] Transform choiceRoot;
+        [SerializeField] Transform choicesParent;
+
+        [SerializeField] GameObject choicePrefarb;
 
         void Start()
         {
             playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
-            AIText.text = playerConversant.GetText();
+            nextButton.onClick.AddListener(Next); // Actions but with different syntax
+            UpdateUI();
         }
+
+        private void Next()
+        {
+            playerConversant.Next();
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            AIText.text = playerConversant.GetText();
+            nextButton.gameObject.SetActive(playerConversant.HasNext());
+            foreach (Transform item in choicesParent)
+            {
+                Destroy(item.gameObject);
+            }
+            foreach (string choiceText in playerConversant.GetChoices())
+            {
+                GameObject option = Instantiate(choicePrefarb, choicesParent);
+                option.GetComponentInChildren<TextMeshProUGUI>().text = choiceText;
+            }
+        }
+
+
     }
 }
